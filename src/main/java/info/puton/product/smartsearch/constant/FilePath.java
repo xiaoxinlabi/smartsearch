230d6102fa72f655e408b5fb2615c2d9548e0468
xiaoxinlabi <769628268@qq.com>
2016-10-10 14:22:30 +0800

    addFilePathConstant

---
+++ src/main/java/info/puton/product/smartsearch/constant/FilePath.java
@@ -0,0 +1,16 @@
+package info.puton.product.smartsearch.constant;
+
+/**
+ * Created by Pauline on 16/10/10.
+ */
+public class FilePath {
+
+    public final static class CACHE {
+
+        public final static String INITIAL = "cache/ini/";
+
+        public final static String PDF = "cache/pdf/";
+
+    }
+
+}
