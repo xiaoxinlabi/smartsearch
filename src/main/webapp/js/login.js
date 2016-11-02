8b8b83bf465795baac1798b2665c22cfd0bdd720
xiaoxinlabi <769628268@qq.com>
2016-11-02 17:03:40 +0800

    登陆页面

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,3 +1,23 @@
-/**
- * Created by Pauline on 16/10/26.
- */
+
+function submit() {
+    var param = {
+        username: $("#username").val(),
+        password: $("#password").val()
+    };
+    $.ajax({
+        type: "post",
+        url: "rest/login",
+        data: param,
+        dataType: "json",
+        success: function (data) {
+            if (data.status == "success") {
+                window.location.href = "admin.html";
+            } else if (data.status = "error") {
+                alert("用户名或密码错误!");
+            }
+        },
+        error: function () {
+            alert("调用失败....");
+        }
+    });
+}
