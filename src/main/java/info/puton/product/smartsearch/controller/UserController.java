1b26b52f9c9be677190381ce3e6bcbf0a5cb7135
xiaoxinlabi <769628268@qq.com>
2016-11-02 16:18:24 +0800

    shiro权限登陆

--- src/main/java/info/puton/product/smartsearch/controller/UserController.java
+++
@@ -1,7 +0,0 @@
-package info.puton.product.smartsearch.controller;
-
-/**
- * Created by Pauline on 16/10/27.
- */
-public class UserController {
-}
--- src/main/webapp/headbar.jsp
+++ src/main/webapp/headbar.jsp
@@ -80,5 +80,5 @@
         </span>
 
     </div>
-
+</body>
 </html>
